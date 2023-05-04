fe58ad68ba9c0de270553aeb62902ea197e7d90f
chetan791 <chetanchauhan795@gmail.com>
2023-05-04 21:50:24 +0530

    day2.2

--- travel/src/App.js
+++ travel/src/App.js
@@ -3,12 +3,7 @@ import Footer from "./Components/Footer";
 import { FlightBooking } from "./Pages/FlightBooking";
 
 function App() {
-  return (
-    <div className="App">
-      <FlightBooking />
-      <Footer />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
