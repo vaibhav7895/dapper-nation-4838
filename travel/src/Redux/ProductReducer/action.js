686ad3b2ecd4faafd4ecee6061e612a6552193e5
Vaibhav <vaibhav7895@gmail.com>
2023-05-04 22:43:09 +0530

    day-1

---
+++ travel/src/Redux/ProductReducer/action.js
@@ -0,0 +1,15 @@
+import { PRODUCT_REQUEST_FAILURE, PRODUCT_REQUEST_PENDING, PRODUCT_REQUEST_SUCCESS } from "./actionTypes"
+import axios from "axios"
+
+export const getProducts=(paramObj)=>(dispatch)=>{
+ dispatch({type:PRODUCT_REQUEST_PENDING})
+ axios.get(`http://localhost:8080/destination`,paramObj).then((res)=>{
+    let obj={
+        data:res.data,
+        total:res.headers.get("X-Total-Count")
+    }
+    dispatch({type:PRODUCT_REQUEST_SUCCESS,payload:obj})
+    }).catch(()=>{
+        dispatch({type:PRODUCT_REQUEST_FAILURE})
+    })
+}
