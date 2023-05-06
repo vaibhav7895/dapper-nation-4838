7e09683ae99da98038983eac20a50a2a58e63527
unknown <aniketghormare62@gmail.com>
2023-05-06 23:07:30 +0530

    new temp commit

--- travel/src/App.js
+++ travel/src/App.js
@@ -1,23 +1,39 @@
 
-import "./App.css";
-import { FlightBooking } from "./Pages/FlightBooking";
-import Navbar from "./Components/Navbar";
-import HomePage from "./Pages/HomePage";
-import Footer from "./Components/Footer";
+import { useSelector } from 'react-redux';
+import './App.css';
+
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
+import MainRoutes from './Pages/MainRoutes';
+import Nav from './Components/Adminnavbar';
+import AdminRoutes from './Pages/Admin/AdminRoutes';
+import { BrowserRouter } from 'react-router-dom';
+import SimpleSidebar from './Components/Sidebar';
+import { Box } from '@chakra-ui/react';
 
 function App() {
+  const admin=useSelector((store)=>{
+    return store.AdminReducer.admin
+  })
+  console.log(admin)
   return (
-    <div className="App">
-
-      
-      <HomePage/>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+    {!admin ? <div className="App">
+      <Navbar />
+      <MainRoutes />
+      <Footer />
+    </div> : <div className="App" style={{display:"flex"}}>
+    <SimpleSidebar style={{width:"20%"}}/>
+    <Box  style={{width:"80%"}}>
+    <Nav />
+      <AdminRoutes />
+    </Box>
+     
+    </div>}
+    </BrowserRouter>
    
-
+  
   );
 }
- 
-    
 
 export default App;
